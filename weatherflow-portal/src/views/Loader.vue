06741c99881320312b9563f8cfe83154f22778d7
Julian Bowman <julianfbowman@gmail.com>
2024-10-01 14:06:56 -0400

    cleaned up loader

--- weatherflow-portal/src/views/Loader.vue
+++ weatherflow-portal/src/views/Loader.vue
@@ -14,22 +14,21 @@
   </script>
 
   <style scoped>
-.loader {
-  border: 16px solid #f3f3f3;
-  border-top: 16px solid #512b59;
-  border-radius: 50%;
-  width: 100px;
-  height: 100px;
-  animation: spin 2s linear infinite;
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
+    .loader {
+        border: 16px solid #f3f3f3;
+        border-top: 16px solid #512b59;
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        animation: spin 2s linear infinite;
+        position: absolute;
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
 
-@keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-}
-
+    @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
   </style>
